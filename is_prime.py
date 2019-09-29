f4402663d00cda6429a8e9d8c9538c0b97d15a2f
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-09-30 07:23:13 +0900

    done

---
+++ is_prime.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+import sys
+sys.setrecursionlimit(10000000)
+
+
+def is_prime(N):
+    """Nを2~int(N**0.5)までで割る"""
+    for i in range(2, int(N**0.5)+1):
+        if N % i == 0:
+            return False
+    return True
+
+
+def main():
+    N = int(input())
+    if is_prime(N):
+        print("YES")
+    else:
+        print("NO")
+
+
+if __name__ == '__main__':
+    main()
