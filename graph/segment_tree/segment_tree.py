b504630dd2a2d0eaf6e575d141d825d605b97e4f
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-10-24 08:28:07 +0900

    ON THE WAY

--- graph/segment_tree/segment_tree.py
+++ graph/segment_tree/segment_tree.py
@@ -1,38 +1,38 @@
 import sys
 sys.setrecursionlimit(10000000)
 
-
 INF = 10 ** 8
 
+
 class SegmentTree():
-    def __init__(self, N, A):
+    def __init__(self, N):
         self.N = N
-        self.nodes = [INF] * (2 * N)
-        self.__init(A)
+        self.nodes = [INF] * (2 * N + 1)
 
 
-def query():
+    def find(self, from_, to_):
+        """
+        from_からto_までの最小値を返す
+        """
+        pass
 
 
-def update():
-
-
-def __init(self, A):
-    # initialize the segment tree
-    # initialize the lists
-    for i in range(N, 2*N):
-        self.nodes[i] = A[i - N]
-    
-    #  initialize the minimum
-    
-    
-
+    def update(self, i, x):
+        """
+        i番目の要素をxに変更する
+        """
+        pass    
 
 
 def main():
-    N = int(input())
-    A = [int(c) for c in input().split()]
-    segment_tree = SegmentTree(N, A)
+    n, q = map(int, input().split())
+    segment_tree = SegmentTree(n)
+    for _ in range(q):
+        com, x, y = map(int, input().split())
+        if com == 0:
+            segment_tree.update(x, y)
+        else:
+            segment_tree.find(x, y)
 
 
 
