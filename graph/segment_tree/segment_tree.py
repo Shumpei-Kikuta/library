c31bb70ae20b26467326f837ca638eecc5db6da9
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-10-25 10:27:36 +0900

    done update

--- graph/segment_tree/segment_tree.py
+++ graph/segment_tree/segment_tree.py
@@ -1,27 +1,46 @@
 import sys
 sys.setrecursionlimit(10000000)
 
-INF = 10 ** 8
+INF = 2 ** 31 - 1
 
 
 class SegmentTree():
     def __init__(self, N):
-        self.N = N
-        self.nodes = [INF] * (2 * N + 1)
+        self.N = 1  # the number of leaves
+        while self.N < N:
+            self.N = 2 * self.N
+        self.nodes = [INF] * (2 * self.N - 1)  # 0 origin
 
 
-    def find(self, from_, to_):
+    def find(self, from_, to_, k, left, right):
         """
-        from_からto_までの最小値を返す
+        return the minimum value between the index of from_ and to_
+        k: which node to look at
         """
-        pass
+        if from_ >= left and to_ <= right:
+            # include
+            return self.nodes[k]
+
+        elif from_ >= left or to_ <= left:
+            # partially include
+            mid = (left + right) // 2
+            return min(self.find(from_, to_, k * 2, left, mid), self.find(from_, to_, k * 2 + 1, mid+1, right))
+        else:
+            # exclude
+            return INF
 
 
     def update(self, i, x):
         """
-        i番目の要素をxに変更する
+        update the element i into x
         """
-        pass    
+        i += self.N - 1
+        self.nodes[i] = x
+        while (True):
+            i = (i - 1) // 2
+            self.nodes[i] = min(self.nodes[i * 2 + 2], self.nodes[i * 2 + 1])
+            if i == 0:
+                break
 
 
 def main():
@@ -32,8 +51,10 @@ def main():
         if com == 0:
             segment_tree.update(x, y)
         else:
-            segment_tree.find(x, y)
-
+            if x > y:
+                x, y = y, x
+            min_ = segment_tree.find(x, y, 1, 1, n)
+            print(min_)
 
 
 if __name__ == '__main__':
